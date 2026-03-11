e639e5d8e82b86ae88270a0a1b34116f4bb02be7
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2026-03-12 05:13:21 +0530

    FIRST

---
+++ src/test/java/com/vibechat/VibeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
